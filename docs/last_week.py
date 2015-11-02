8675bb59fd67ef86dd1158e443f17f8200073e24
John Lockwood <elitepropertiesbroker@gmail.com>
2015-11-01 20:37:51 -0800

    Hours, and new hours stats program

---
+++ docs/last_week.py
@@ -0,0 +1,18 @@
+from dateutil.parser import parse
+from datetime import datetime, timedelta
+now = datetime.now()
+now = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+from json import loads
+with open('hours.json', 'r') as f:
+    read_data = f.read()
+data = loads(read_data)
+total_hours_this_week = 0
+for i in data["hours"]:
+	the_date = parse(i["date"])
+	delta_t = now - the_date
+	print("time_diff:", delta_t.days, "hours:", i["hours"])
+	if delta_t.days <= 7:
+		total_hours_this_week = total_hours_this_week + i["hours"]
+
+print("Total hours this week: ", total_hours_this_week)
