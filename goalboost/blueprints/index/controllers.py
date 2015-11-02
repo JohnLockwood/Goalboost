11b75c12ea05ae59eaa7256e5d5cd08a5bb46288
John Lockwood <elitepropertiesbroker@gmail.com>
2015-11-01 16:47:05 -0800

    Add foundation

--- goalboost/blueprints/index/controllers.py
+++ goalboost/blueprints/index/controllers.py
@@ -5,9 +5,11 @@ from flask import Blueprint, redirect, render_template
 mod_index = Blueprint('index', __name__, url_prefix='/')
 
 # Set the route and accepted methods
-@mod_index.route('/', methods=['GET'])
+@mod_index.route('', methods=['GET'])
 def index():
     return render_template("index.html")
 
-
+@mod_index.route('foundation', methods=['GET'])
+def fnd_eg():
+    return render_template("home/foundation_example.html")
 
