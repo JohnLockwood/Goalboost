159f10bd7137328409bef51af4a1877ecc134c2b
John Lockwood <elitepropertiesbroker@gmail.com>
2016-01-09 21:41:33 -0800

    Continue to write and test MongoMintDocument class.

--- goalboost/model/mongomint.py
+++ goalboost/model/mongomint.py
@@ -1,45 +1,70 @@
-# mongo mint, because it's wafer thin, and hence, pythonic
-
+"""
+MongoMintDocument is a wafer thin wrapper around a PyMongo collection
+"""
+from bson import ObjectId
 
 class MongoMintDocument(object):
 
-    # Initialize with a connection, which is a PyMongoClient instance
-    def __init__(self, pymongo_client_object, collection, database="goalboost"):
+    """Create a MongoMintDocument object.
+
+    pymongo_client_object - a MongoClient instance
+    collection_name - a name for the collection we want to validate on / operate against
+    database (optional, default="goalboost"), a name for the database where the collection will live
+    """
+    def __init__(self, pymongo_client_object, collection_name, database="goalboost"):
         self.connection = pymongo_client_object
         self.database = database
-        self.collection = collection
+        self.collection_name = collection_name
         self._clear_validation_errors()
 
-    # TODO finish
+    """upsert - easy single document save.
+
+    If validate generates no error, then insert a new document or update
+    the existing one, returning true.
+
+    If validate did return an error, return false.  In that case. self.errors can
+    be inspected to find what happened.
+    """
     def upsert(self, model_dictionary):
         if self._is_valid(model_dictionary):
-            if "_id" in model_dictionary:   # Update case
-                return True
-            else:
-                return True
+            if "_id" in model_dictionary:               # Update case
+                self.collection.replace_one( \
+                    filter = {"_id" : model_dictionary["_id"]}, replacement=model_dictionary)
+            else:                                       # Insert case
+                self.collection.insert_one(model_dictionary)
+            return True
         else:
             return False
 
-    # TODO
-    def json_to_dictionary(self, json):
+    """find_by_id - Return a single document by id or None, accepting either string or ObjectId as argument"""
+    # Always create an ObjectId, correctly handles both string case and ObjectId case
+    def find_by_id(self, id):
+        return self.collection.find_one({"_id": ObjectId(id) })
+
+    """collection (property)
+
+    Easily use the underlying collection to use native pymongo methods, e.g. drop, find_one, etc.
+    """
+    @property
+    def collection(self):
+        return self.connection[self.database][self.collection_name]
+
+
+    """validate (optional override allows you to provide pre-save data validation
+
+    To implement if needed, add one string per validation failure to self.errors.
+
+    """
+    def validate(self, model_dictionary):
         pass
 
-    def get_collection(self):
-        return self.connection[self.database][self.collection]
 
+    # Private methods
     def _clear_validation_errors(self):
         self.errors = []
-
-    def get_validation_errors(self):
-        return self.errors
 
     def _is_valid(self, model_dictionary):
         self._clear_validation_errors()
         self.validate(model_dictionary)
-        return len(self.errors) > 0
+        return len(self.errors) == 0
 
-    # If you don't want data validation, don't implement
-    # If you do implement, then add one string per validation failure to
-    # self.errors
-    def validate(self, model_dictionary):
-        pass
--- test/unit/model/test_mongomint.py
+++ test/unit/model/test_mongomint.py
@@ -1,9 +1,19 @@
 from unittest import TestCase
 
+from bson import ObjectId
+
 from goalboost.model.mongomint import MongoMintDocument
 from test.common.test_helper import TestHelper
 from goalboost.model import db
 from pymongo import MongoClient
+
+class SampleDocumentClass(MongoMintDocument):
+    def validate(self, model_dictionary):
+        if ("valid" not in model_dictionary):
+            self.errors.append("Valid is a required field")
+        elif "invalid" in model_dictionary:
+            self.errors.append("Who stuck this invalid field on here?")
+
 
 class TestMongoMintDocument(TestCase):
 
@@ -41,22 +51,62 @@ class TestMongoMintDocument(TestCase):
 
     def test_can_do_object_upsert(self):
         doc = MongoMintDocument(db.connection, "mint_object_upsert")
-        connection = doc.connection
         model1 = dict(name="John")
         assert("_id" not in model1)
+        doc.upsert(model1)
+        assert("_id" in model1)
 
-        #
-        # collection.insert_one(model1)
-        # assert("_id" in model1)
-        # model2 = collection.find_one()
-        # assert(model1["name"] == model2["name"])
-        # assert(model1["_id"] == model2["_id"])
-        # model1["name"] = "Jenniffer"
-        # collection.replace_one(filter = {"_id" : model1["_id"]}, replacement=model1)
-        #
-        # model3 = model2 = collection.find_one(model1)
-        # assert(model3["name"] == "Jenniffer")
-        # assert(model3["_id"] == model1["_id"])
-        # collection.drop()
+        model1["name"] = "Jenniffer"
+
+        doc.upsert(model1)
+
+        # JCL todo wrap in method
+        model2 = doc.collection.find_one({"_id" : model1["_id"]})
+        assert(model2["_id"] == model1["_id"])
+        assert(model2["name"] == "Jenniffer")
+
+        doc.collection.drop()
+
+    def test_can_find_by_id(self):
+        doc = MongoMintDocument(db.connection, "mint_object_upsert")
+        genius = "Spencer Reid"
+        model1 = dict(name=genius)
+        doc.upsert(model1)
+        id = model1["_id"]
+        assert(type(id) == ObjectId)
+
+        # Triial case, already right type:
+        model2 = doc.find_by_id(id)
+        assert(model2["name"] == genius)
+
+        # Validate can load by string
+        # Convert to string first
+        model3 = doc.find_by_id(str(id))
+        assert(model3["name"] == genius)
+        doc.collection.drop()
+
+    def test_validation(self):
+        doc = SampleDocumentClass(db.connection, "mint_object_validation")
+
+        model1 = dict(valid="Yes, this will be valid because key there")
+        assert(True == doc.upsert(model1))
+        assert(len(doc.errors) == 0)
+
+
+        model1 = dict(valid="This would have been valid based on this", invalid="But this makes it invalid")
+        assert(False == doc.upsert(model1))
+        assert(len(doc.errors) == 1)
+
+        # This doesn't contain the valid tag, so invalid
+        model2 = dict(fish="Wanda")
+        assert(False == doc.upsert(model2))
+        assert(len(doc.errors) == 1)
+        assert("required" in doc.errors[0])
+
+
+
+        doc.collection.drop()
+
+
 
 
